e709457dcc166e1c269e45bccf1de27f62417d2b
djirufin <rufin.dji@gmail.com>
2020-02-12 18:51:30 +0000

    First commit

---
+++ reports.md
@@ -0,0 +1,36 @@
+---
+title: "Application reports"
+author: "MyAI"
+date: 'Feb 12 2020'
+output: html_document
+---
+
+
+
+
+
+
+
+# Application Reports Wed/Feb/122020 18:47:47 
+
+
+
+|MODULE            | YESTERDAY|   TODAY| DIFFERENCE|   PCT|
+|:-----------------|---------:|-------:|----------:|-----:|
+|BUNDLES3G         |   3870500| 4208200|     337700| 108.7|
+|WAUU              |   3823075| 3909100|      86025| 102.3|
+|YOUTHv4           |   3174200| 3305450|     131250| 104.1|
+|BOMBASTICO        |   2017900| 2032700|      14800| 100.7|
+|MTNLIVRE          |   2011200| 1905700|    -105500|  94.8|
+|BUNDLES3G_EVD_123 |    768700|  813500|      44800| 105.8|
+|INTLBUNDLE        |    350800|  350800|          0| 100.0|
+|EVDMTNLIVRE       |    118400|   80200|     -38200|  67.7|
+|EVDZEROSTRESS     |     27900|   30780|       2880| 110.3|
+|EVDDATA           |     22000|   30000|       8000| 136.4|
+|CRAZYSUNDAY       |     28100|   27850|       -250|  99.1|
+|EVDBOMBASTICO     |     12700|   18000|       5300| 141.7|
+|EVDJOVCOOL        |      2700|    6100|       3400| 225.9|
+|HELLOWORLD        |      3300|    2000|      -1300|  60.6|
+|SMSBUNDLE         |       800|     700|       -100|  87.5|
+
+
---
+++ source_reports.R
@@ -0,0 +1,50 @@
+library(dplyr)
+library(RMySQL)
+library(RJDBC)
+
+#sQL for MySQL
+query_mysql <- "SELECT CASE A.MODULE WHEN 'BUNDLES3G' then 'BUNDLES3G_EVD_123' ELSE A.MODULE END MODULE, B.TOTAL YESTERDAY, A.TOTAL TODAY, (A.TOTAL - B.TOTAL) DIFFERENCE, ROUND(A.TOTAL*100/(B.TOTAL), 1) PCT 
+FROM (SELECT MODULE, COUNT(DISTINCT MSISDN) DIST ,COUNT(BUNDLE_PRICE) NBRE, SUM(BUNDLE_PRICE) TOTAL FROM COMMON.CDR
+WHERE TRANSDATE = CURDATE() AND STATUS_REASON = 'BUNDLE_SUCCESS' AND LAST_UPDATE <= current_timestamp() GROUP BY MODULE) A 
+LEFT JOIN (SELECT MODULE, COUNT(DISTINCT MSISDN)DIST,COUNT(BUNDLE_PRICE) NBRE, SUM(BUNDLE_PRICE) TOTAL FROM COMMON.CDR
+WHERE TRANSDATE = date_sub(CURDATE(), INTERVAL 1 DAY) AND STATUS_REASON = 'BUNDLE_SUCCESS' AND LAST_UPDATE <= date_sub(current_timestamp(), interval 1 day)
+GROUP BY MODULE) B USING (MODULE) ORDER BY PCT DESC"
+
+#SQL for oracle
+query_orcl <- "SELECT MODULE, B.TOTAL YESTERDAY, A.TOTAL TODAY, (A.TOTAL - B.TOTAL) DIFFERENCE, ROUND(A.TOTAL*100/(B.TOTAL), 1) PCT 
+FROM (SELECT MODULE, COUNT(DISTINCT MSISDN) DIST ,COUNT(BUNDLE_PRICE) NBRE, SUM(BUNDLE_PRICE) TOTAL FROM CDR
+WHERE TRANSDATE = trunc(sysdate) AND STATUS_REASON = 'BUNDLE_SUCCESS' AND LAST_UPDATE <= sysdate GROUP BY MODULE) A 
+LEFT JOIN (SELECT MODULE, COUNT(DISTINCT MSISDN)DIST,COUNT(BUNDLE_PRICE) NBRE, SUM(BUNDLE_PRICE) TOTAL FROM CDR
+WHERE TRANSDATE = trunc(sysdate-1) AND STATUS_REASON = 'BUNDLE_SUCCESS' AND LAST_UPDATE <= sysdate-1
+GROUP BY MODULE) B USING (MODULE) ORDER BY PCT DESC"
+
+#Connection to server 39
+myDB39 <- dbConnect(MySQL(), user='everaldo', password='123456', dbname='COMMON', host='192.168.100.39')
+rs39 <- dbSendQuery(myDB39, query_mysql)
+data39 <- fetch(rs39, n=-1)
+
+
+#Connection to server 15
+myDB15 <- dbConnect(MySQL(), user='mtn', password='Mtn@123456', dbname='COMMON', host='192.168.100.15')
+rs15 <- dbSendQuery(myDB15, query_mysql)
+data15 <- fetch(rs15, n=-1)
+
+#Connection to oracle server
+drv <- JDBC("oracle.jdbc.OracleDriver", classPath = "lib/ojdbc6.jar")
+orcl <- dbConnect(drv, "jdbc:oracle:thin:@//10.100.2.179:1521/vasdb", "vas", "vas")
+dataOrcl <- dbGetQuery(orcl, query_orcl)
+
+#bind dataframes
+sdf <- rbind(data39, data15, dataOrcl)
+rm(data39, data15, dataOrcl)
+
+#Mutate final sdf and add datetime
+sdf <- sdf %>% mutate("Current DateTime" = format(Sys.time(), "%a %b %d %X %Y"))
+
+
+#Clear resultset and close connections
+dbClearResult(dbListResults(myDB15)[[1]])
+dbClearResult(dbListResults(myDB39)[[1]])
+dbDisconnect(myDB15)
+dbDisconnect(myDB39)
+dbDisconnect(orcl)
