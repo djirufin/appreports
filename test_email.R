f090c4ae5b96cd8c227369d5f2151e06e0138a51
djirufin <rufin.dji@gmail.com>
2020-02-13 19:03:44 +0000

    Loading all exco member

--- test_email.R
+++ test_email.R
@@ -3,8 +3,12 @@ library(knitr)
 knit2html("reports.Rmd", options = "", force_v1 = T)
 
 send.mail(from = "monitoring.gw@mtn.com", 
-          
-          to=c("mehibo.rufindji@mtn.com"),
+          to=c("mehibo.rufindji@mtn.com", "likadeu.sahi@mtn.com", "Cyrille.Babatounde2@mtn.com", 
+               "patrice.dje@mtn.com", "Gaspard.Bayigane@mtn.com", "Reuben.Opata@mtn.com",
+               "Freddie.Mokoena@mtn.com", "Jane.Mugenyi@mtn.com", "Bubacar.FalCamara@mtn.com", "Luisa.Rosa@mtn.com",
+               "Wilfredo.Fernandes@mtn.com", "Etiandro.Ucha@mtn.com"
+               ), 
+          #to=c("mehibo.rufindji@mtn.com"),
           subject = paste0("Bundles reports H2H: ", format(Sys.time(), "%m/%d/%Y at %X", tz="Africa/Bissau")),
 body = "reports.html", html=T,
 smtp=list(host.name="smtp.office365.com", port=587, user.name="monitoring.gw@mtn.com", passwd="IMiE8XaK!DfZ", tls=T),
